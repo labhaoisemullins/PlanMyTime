178f22891be4ec3982a77abe8fd940ff590fad33
Labhaoise Mullins <labhaoise45@gmail.com>
2023-03-19 18:25:00 +0000

    Add Sign Up page

---
+++ app/src/main/java/ie/ul/planmytime/UserSignUp.java
@@ -0,0 +1,47 @@
+package ie.ul.planmytime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class UserSignUp extends AppCompatActivity {
+
+    private Button SignUpBtn;
+    private Button redirectSignInBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_up);
+
+        //  On Click - open main page of app
+        SignUpBtn = (Button) findViewById(R.id.SignInBtn);
+        SignUpBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openHomepage();
+            }
+        });
+
+        // On Click - Redirected to Sign Up Page
+        redirectSignInBtn = (Button) findViewById(R.id.redirectSignInBtn);
+        redirectSignInBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSignUpPage();
+            }
+        });
+    }
+    public void openHomepage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSignUpPage(){
+        Intent intent = new Intent(this, UserSignUp.class);
+        startActivity(intent);
+    }
+}
