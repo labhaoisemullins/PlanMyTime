1cfaee0f8b2c0beb20fa4cfc1d7a668ece362eba
Labhaoise Mullins <labhaoise45@gmail.com>
2023-04-30 10:06:31 +0100

    remove whitespace

--- app/src/main/java/ie/ul/planmytime/UserSignUp.java
+++ app/src/main/java/ie/ul/planmytime/UserSignUp.java
@@ -21,8 +21,11 @@ public class UserSignUp extends AppCompatActivity {
     private Button redirectSignInBtn;
 
     TextInputEditText inputName;
+
     TextInputEditText inputEmail;
     TextInputEditText inputPassword;
+
+
     FirebaseAuth mAuth;
 
     @Override
@@ -76,3 +79,32 @@ public class UserSignUp extends AppCompatActivity {
         }
     }
 }
+
+        //  On Click - open main page of app
+//        SignUpBtn = (Button) findViewById(R.id.SignInBtn);
+//        SignUpBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                openHomepage();
+//            }
+//        });
+
+        // On Click - Redirected to Sign Up Page
+//        redirectSignInBtn = (Button) findViewById(R.id.redirectSignInBtn);
+//        redirectSignInBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                openSignUpPage();
+//            }
+//        });
+//    }
+//    public void openHomepage(){
+//        Intent intent = new Intent(this, Homepage.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openSignUpPage(){
+//        Intent intent = new Intent(this, UserSignUp.class);
+//        startActivity(intent);
+//    }
+
